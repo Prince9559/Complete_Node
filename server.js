0a256137ce1ab116f0474782996b73b528a475f6
Prince Software <kumarprince95596186@gmail.com>
2026-07-01 15:14:19 +0530

    authtication

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 require('dotenv').config();
-
-
+const passport=require('./auth');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT =process.env.PORT || 3000;
 
-const PORT =process.env.PORT || 5500;
+
+const logRequest =(req,res,next)=>{
+  // console.log(`${new Date().toLocaleString()} Request Mode to : ${req.originalUrl}`);
+  next();
+}
+
+app.use(logRequest);
+app.use(passport.initialize());
+const localAuthMiddleware=passport.authenticate('local',{session:false})
+
+app.get('/',function(req,res){
+  res.send("Welcome to our Hotel");
+})
+
 
 const personRouter=require('./routes/personRouter');
 const menuRouter=require('./routes/menuRouter');
+
 
 app.use('/person',personRouter);
 app.use('/menu',menuRouter);
